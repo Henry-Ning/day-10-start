c19740a9870aec17bea83ad0ef97420942675823
Henry Ning <henryhuning@gmail.com>
2021-02-04 19:39:43 +0000

    98. Multiple return values 4min

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
-def format_name(first_name, last_name):
-  formatted_first_name = first_name.title()
-  formatted_last_name = last_name.title()
-  return formatted_first_name +" "+ formatted_last_name
+#Functions with Outputs
+def format_name(f_name, l_name):
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  f"Result: {formated_f_name} {formated_l_name}"
 
-print(format_name("HenrY", "NING"))
+#Storing output in a variable
+formatted_name = format_name(input("Your first name: "), input("Your last name: "))
+print(formatted_name)
+#or printing output directly
+print(format_name(input("What is your first name? "), input("What is your last name? ")))
 
-# 2-3 3 am
+#Already used functions with outputs.
+length = len(formatted_name)
+
+#Return as an early exit
+def format_name(f_name, l_name):
+  """Take a first and last name and format it 
+  to return the title case version of the name."""
+  if f_name == "" or l_name == "":
+    return "You didn't provide valid inputs."
+  formated_f_name = f_name.title()
+  formated_l_name = l_name.title()
+  return f"Result: {formated_f_name} {formated_l_name}"
+
+
